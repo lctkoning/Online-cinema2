bbdc5d3a126c858785b018ccd36394762f5ae581
Lex Koning <lex.koning@devoteam.com>
2021-02-03 15:26:03 +0100

    new commit

--- test1.py
+++ test1.py
@@ -2,39 +2,30 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-
-chrome_options.add_argument("--headless")
+import pytest
+import sys
 
 
-chrome_options.binary_location = "/opt/google/chrome/chrome"    #chrome binary location specified here
 
-chrome_options.add_argument("--no-sandbox") #bypass OS security model
-chrome_options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
-chrome_options.add_argument("--disable-gpu")
+def test_lambdatest_todo_app():
+    chrome_options = Options()
 
+    chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=chrome_options)  # Optional argument, if not specified will search path.
+    chrome_options.binary_location = "/opt/google/chrome/chrome"  # chrome binary location specified here
 
-start_url = 'http://localhost:8080/cinema/movies/page/1'
-driver.get(start_url)
+    chrome_options.add_argument("--no-sandbox")  # bypass OS security model
+    chrome_options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+    chrome_options.add_argument("--disable-gpu")
 
-def login_test():
+    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)  # Optional argument, if not specified will search path.
+
+    start_url = 'http://localhost:8080/cinema/movies/page/1'
+    driver.get(start_url)
     driver.find_element_by_id('username').send_keys('admin')
     driver.find_element_by_id('password').send_keys('admin')
     driver.find_element_by_name('submit').click()
     print ("Login works!")
-    
-login_test()
-
-def movielisttest():
-    movielist = driver.find_elements_by_class_name('title')
-
-    if len(movielist) == 5:
-        print('Number of movies are correct')
-
-movielisttest()
-
-
-driver.quit()
+    driver.find_elements_by_class_name('title')
+    sleep(2)
+    driver.close()
---
+++ testnew2.py
@@ -0,0 +1,36 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import pytest
+import sys
+from time import sleep
+
+
+class Testcinema():
+    @pytest.fixture()
+    def setup(self):
+        chrome_options = Options()
+
+        chrome_options.add_argument("--headless")
+
+        chrome_options.binary_location = "/opt/google/chrome/chrome"  # chrome binary location specified here
+
+        chrome_options.add_argument("--no-sandbox")  # bypass OS security model
+        chrome_options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+        chrome_options.add_argument("--disable-gpu")
+
+        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)  # Optional argument, if not specified will search path.
+        yield
+        self.driver.close()
+        #start_url = 'http://localhost:8080/cinema/movies/page/1'
+        #driver.get(start_url)
+        
+    def test_login(self, setup):
+        self.driver.get('http://localhost:8080/cinema/movies/page/1')
+        self.driver.find_element_by_id('username').send_keys('admin')
+        self.driver.find_element_by_id('password').send_keys('admin')
+        self.driver.find_element_by_name('submit').click()
+
+    def test_movies(self, setup):
+        self.driver.find_elements_by_class_name('title')
